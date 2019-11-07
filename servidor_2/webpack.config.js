455a83ddd9e77ba825c240da39294c15c2adb23c
soldi3rofwarz <josejehovanicastroperez@gmail.com>
2019-11-07 17:27:54 -0600

    haciendo servidor

---
+++ servidor_2/webpack.config.js
@@ -0,0 +1,65 @@
+const MiniCssExtractPlugin =require('mini-css-extract-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports={
+    entry: './src/frontend/index.js',
+    output:{
+        path:path.resolve(__dirname,'app_compilada'),
+        filemane:'bundle.js',
+    },
+    resolve:{
+        extensions:['.js','.jsx']
+    },
+    module:{
+            rules:[{
+                test: /\.(js|jsx)$/,
+                exclude:/node_modules/,
+                use: {
+                    loader:'babel-loader',
+                },
+            },
+            {
+                test:/\.html$/,
+                use: {
+                    loader:'html-loader',
+                },
+            },
+            {
+                test:/\.(s*)css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                    },
+                ],
+            },
+            {
+                test:/\.(png|gif|jpg)$/,
+                use: [
+                    {
+                        'loader': 'file-loader', 
+                    
+                        options:{
+                    
+                            name:'assets/resources/[name].[ext]',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./public/index.html',
+            filename:'./index.html',
+        }),
+
+        new MiniCssExtractPlugin({
+            filename:'assets/app.css',
+        }),
+    ],
+};

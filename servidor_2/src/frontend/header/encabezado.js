d421ab61a33a7d9b0f754d959df8f5021e39bcb5
soldi3rofwarz <josejehovanicastroperez@gmail.com>
2019-11-26 12:17:34 -0600

    subiendo header

--- servidor_2/src/frontend/header/encabezado.js
+++ servidor_2/src/frontend/header/encabezado.js
@@ -1,23 +1,41 @@
+
 import React from 'react';
-import imagen from './../../assets/imagen.png';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
-const Encabezado = () => {
-    return (
-        <header className = 'contentall'> 
-            <img className = 'imagen'
-            src = {imagen}
-            />
-        <nav>
-             <ul>
-                <li><a >Inicio</a></li>
-                <li><a >Contacto</a></li>
-                <li><a >vista</a></li>
-                <li><a >vista2</a></li>
-            </ul>
-        </nav>
-    </header>
-    )
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
-
-export default Encabezado;

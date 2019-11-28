b61e281a29a5e5c3c953ae0512f459b1b7ef39d3
estherd16 <sestherduarte@gmail.com>
2019-11-28 17:03:26 -0600

    segunda parte fire base

---
+++ servidor_2/src/lib/data/casas-data.js
@@ -0,0 +1,34 @@
+import db from './firestore-config';
+
+const CLAVE_PUBLICACIONES = 'publicaciones';
+
+export const obtenerPublicaciones = async () => {
+    const querySnapShot = await db.collection(CLAVE_PUBLICACIONES).get();
+    const publicaciones = [];
+    querySnapShot.forEach((documento)=> {
+        publicaciones.push({
+            id: documento.id,
+            ...documento.data(),
+        });
+    });
+    return publicaciones;
+}
+    export const guardarPublicacion = async (publicacion) => {
+        await db.collection(CLAVE_PUBLICACIONES).add(
+            publicacion
+        );
+    }
+
+    export const actualizarPublicacion = async (publicacionId, publicacion) => {
+        await db.collection(CLAVE_PUBLICACIONES).doc(publicacionId).update(
+            publicacion
+        );
+    }
+
+    export const eliminarPublicacion = async (publicacionId) => {
+        await db.collection(CLAVE_PUBLICACIONES).doc(publicacionId).delete();
+    }
+
+    
+
+

ce6e38a91f67c97b0ff2de3ba476c5638006f681
estherd16 <sestherduarte@gmail.com>
2019-12-02 12:09:52 -0600

    cambios

--- servidor_2/src/frontend/routes/server-routes.js
+++ servidor_2/src/frontend/routes/server-routes.js
@@ -1,16 +1,22 @@
 import Home from './../tema/productos/componentes/casas.js';
 import Login from './../tema/home/componentes/login.js';
+import drawer from './../../frontend/drawer/drawer.js';
+
 const serverRoutes = [
     {
         path: '/',
         component:Home,
         exact: true,
-
-        
     },
     {
         path: '/Login',
         component:Login,
+        exact: true,
+    },
+
+    {   
+        path: '/drawer',
+        component: drawer,
         exact: true,
     },
 ];

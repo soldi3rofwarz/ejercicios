5671f8af68ff6308f9a61e2b005585eba89d2042
soldi3rofwarz <josejehovanicastroperez@gmail.com>
2019-12-03 13:20:41 -0600

    componiendo server routes

--- servidor_2/src/frontend/routes/server-routes.js
+++ servidor_2/src/frontend/routes/server-routes.js
@@ -1,9 +1,5 @@
 import Home from './../tema/productos/componentes/casas.js';
-<<<<<<< HEAD
 import Login from './../tema/home/componentes/login'
-=======
-import Login from './../tema/home/componentes/login.js';
->>>>>>> 7cf96003ad69f84e2b2c2ed5fcdafa87a8e6b26f
 const serverRoutes = [
     {
         path: '/',

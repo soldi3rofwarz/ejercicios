c4b0ac42aa353a3c97c64491fd374ac5502e7db8
Jordy <alvarezjordi609@gmail.com>
2019-12-03 16:03:41 -0600

    configuracion de archivos

--- servidor_2/src/frontend/routes/server-routes.js
+++ servidor_2/src/frontend/routes/server-routes.js
@@ -1,9 +1,5 @@
 import Home from './../tema/productos/componentes/casas.js';
-<<<<<<< HEAD
 import Login from './../tema/home/componentes/login.js';
-=======
-import Login from './../tema/home/componentes/login.js'
->>>>>>> 9859e9b694ea84e4daea66c664ecabf42179955f
 const serverRoutes = [
     {
         path: '/',
@@ -15,18 +11,7 @@ const serverRoutes = [
         path: '/Login',
         component:Login,
         exact: true,
-<<<<<<< HEAD
     }, 
-s];
-=======
-    },
-
-    /* {   
-        path: '/drawer',
-        component: drawer,
-        exact: true,
-    }, */
 ];
->>>>>>> 9859e9b694ea84e4daea66c664ecabf42179955f
 
 export default serverRoutes;

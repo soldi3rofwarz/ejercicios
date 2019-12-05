fa499a52d085e6dfb65b881233e35d223b49cf13
soldi3rofwarz <josejehovanicastroperez@gmail.com>
2019-12-04 19:49:12 -0600

    resubiendo home y home-comp

---
+++ servidor_2/src/frontend/tema/home/componentes/login/home-comp.js
@@ -0,0 +1,49 @@
+import React ,{Component} from 'react';
+import Image1 from './../../../../assets/apartamento.jpg';
+import Image2 from './../../../../assets/c1.jpg';
+import Image3 from './../../../../assets/c2.jpg';
+import Image4 from './../../../../assets/c3.jpg';
+import Image5 from './../../../../assets/csa2.jpg';
+import Image6 from './../../../../assets/c5.jpg';
+import { Component } from 'react';
+
+const Image =[
+
+  {
+    img: Image1,
+    title: 'Casa',
+    precio: '$ 50,000'
+
+  },
+  {
+    img: Image2,
+    title: 'Departamento',
+    precio: '$ 10,000'
+
+  },
+  {
+    img: Image3,
+    title: 'Casa',
+    precio: '$ 20,000'
+
+  },
+  {
+    img: Image4,
+    title: 'Terreno',
+    precio: '$ 80,000'
+
+  },
+  {
+    img: Image5,
+    title: 'Departamento',
+    precio: '$ 60,000'
+
+  },
+  {
+    img: Image6,
+    title: 'Departamento',
+    precio: '$ 40,000'
+
+  },
+
+]

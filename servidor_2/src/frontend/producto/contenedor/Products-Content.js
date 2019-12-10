afa8d39b3efe87c402bdcf306105a1bc2b8fc5ff
soldi3rofwarz <josejehovanicastroperez@gmail.com>
2019-12-10 12:37:52 -0600

    agregando precio

--- servidor_2/src/frontend/producto/contenedor/Products-Content.js
+++ servidor_2/src/frontend/producto/contenedor/Products-Content.js
@@ -11,71 +11,88 @@ class ProductsContent extends Component {
             {
                 imagen: require('./../../../assets/c1.jpg'),
                 titulo: 'casa1',
+                precio: '$ 3000',
             },
             {
                 imagen: require('./../../../assets/c2.jpg'),
                 titulo: 'casa2',
+                precio: '$ 3000',
             },
             {
                 imagen: require('./../../../assets/c3.jpg'),
                 titulo: 'casa3',
+                precio: '$ 3000',
             },
             {
                 imagen: require('./../../../assets/c5.jpg'),
                 titulo: 'casa3',
+                precio: '$ 3000',
             },
             {
                 imagen: require('./../../../assets/c1.jpg'),
                 titulo: 'casa4',
+                precio: '$ 3000',
             },
             {
                 imagen: require('./../../../assets/c1.jpg'),
                 titulo: 'casa5',
+                precio: '$ 3000',
             },
             {
                 imagen: require('./../../../assets/c1.jpg'),
                 titulo: 'casa6',
+                precio: '$ 3000',
             },
             {
                 imagen: require('./../../../assets/c1.jpg'),
                 titulo: 'casa7',
+                precio: '$ 3000',
             },
             {
                 imagen: require('./../../../assets/c1.jpg'),
                 titulo: 'casa8',
+                precio: '$ 3000',
             },
             {
                 imagen: require('./../../../assets/c1.jpg'),
                 titulo: 'casa9',
+                precio: '$ 3000',
             },
             {
                 imagen: require('./../../../assets/c1.jpg'),
                 titulo: 'casa10',
+                precio: '$ 3000',
             },
 
             {
                 imagen: require('./../../../assets/c1.jpg'),
                 titulo: 'casa11',
+                precio: '$ 3000',
             },
             {
                 imagen: require('./../../../assets/c1.jpg'),
                 titulo: 'casa12',
+                precio: '$ 3000',
             },
             {
                 imagen: require('./../../../assets/c1.jpg'),
                 titulo: 'casa13',
+                precio: '$ 3000',
             },
             {
                 imagen: require('./../../../assets/c1.jpg'),
                 titulo: 'casa14',
+                precio: '$ 3000',
             },
             {
                 imagen: require('./../../../assets/c1.jpg'),
                 titulo: 'casa15',
+                precio: '$ 3000',
             },
             {
                 imagen: require('./../../../assets/c1.jpg'),
                 titulo: 'casa16',
+                precio: '$ 3000',
             },
             
         ];
